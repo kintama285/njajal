477ec29f08265cd188654ce95f06cbffc211d173
kintama285 <reyvandi141@students.amikom.ac.id>
2024-03-05 08:06:34 +0700

    first commit

---
+++ p1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 6;
+            Console.WriteLine("hasil penambahan "+a+" + "+b "=" + Penambahan(a,b));
+            Console.WriteLine("hasil pengurangan {0} - {1} = {2}",a,b pengurangan (a,b));
+            Console.WriteLine()
+        }
+        static int Penambahan(int a, int b)
+
+        {
+            return a + b;
+        }
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
